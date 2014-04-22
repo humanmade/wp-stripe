ffe4edc7fd6b92dfba996d28f62b41c08482d67e
Tom Willmot <tom@humanmade.co.uk>
2014-04-22 15:56:25 +0100

    Start re-factoring custom post type and options files

--- includes/stripe-cpt.php
+++ includes/stripe-cpt.php
@@ -4,49 +4,34 @@
 // Custom Post Type - Projects
 
 function create_wp_stripe_cpt_trx() {
-    
-    $labels = array(
-        'name'                  => _x('Stripe Payments', ''),
-        'singular_name'         => _x('Payment', 'post type singular name'),
-        'add_new'               => _x('Add New', 'Payments'),
-        'add_new_item'          => __('Add New Payment'),
-        'edit_item'             => __('Edit Payment'),
-        'new_item'              => __('New Payment'),
-        'view_item'             => __('View Payment'),
-        'search_items'          => __('Search Payments'),
-        'not_found'             => __('No Payments found'),
-        'not_found_in_trash'    => __('No Payments found in Trash'),
-        'parent_item_colon'     => '',
-    );
 
     $args = array(
-        'labels' 		    => $labels,
         'public' 		    => false,
         'can_export' 	    => true,
         'capability_type'   => 'post',
         'hierarchical' 	    => false,
-        'supports'		    => array( 'title', 'editor' )
     );
 
     register_post_type( 'wp-stripe-trx', $args);
-    
+
 }
+add_action( 'init', 'create_wp_stripe_cpt_trx' );
 
 // Custom Post Type - Projects
 
 function create_wp_stripe_cpt_projects() {
 
     $labels = array(
-        'name'                  => _x('Stripe Projects', ''),
-        'singular_name'         => _x('Project', 'post type singular name'),
-        'add_new'               => _x('Add New', 'Payments'),
-        'add_new_item'          => __('Add New Project'),
-        'edit_item'             => __('Edit Project'),
-        'new_item'              => __('New Project'),
-        'view_item'             => __('View Project'),
-        'search_items'          => __('Search Projects'),
-        'not_found'             => __('No Projects found'),
-        'not_found_in_trash'    => __('No Projects found in Trash'),
+        'name'                  => _x( 'Stripe Projects', '', 'wp-stripe' ),
+        'singular_name'         => _x( 'Project', 'post type singular name', 'wp-stripe' ),
+        'add_new'               => _x( 'Add New', 'Payments', 'wp-stripe' ),
+        'add_new_item'          => __( 'Add New Project', 'wp-stripe' ),
+        'edit_item'             => __( 'Edit Project', 'wp-stripe' ),
+        'new_item'              => __( 'New Project', 'wp-stripe' ),
+        'view_item'             => __( 'View Project', 'wp-stripe' ),
+        'search_items'          => __( 'Search Projects', 'wp-stripe' ),
+        'not_found'             => __( 'No Projects found', 'wp-stripe' ),
+        'not_found_in_trash'    => __( 'No Projects found in Trash', 'wp-stripe' ),
         'parent_item_colon'     => '',
     );
 
@@ -62,8 +47,4 @@ function create_wp_stripe_cpt_projects() {
     register_post_type( 'wp-stripe-projects', $args);
 
 }
-
-add_action( 'init', 'create_wp_stripe_cpt_trx' );
-add_action( 'init', 'create_wp_stripe_cpt_projects' );
-
-?>
+//add_action( 'init', 'create_wp_stripe_cpt_projects' );
